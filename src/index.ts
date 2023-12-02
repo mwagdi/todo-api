07d18894827d80e612e57331cc6e37f2d26701ce
Mamoud Elawadi <mahmoudwagdi86@gmail.com>
2023-12-02 10:30:20 +0100

    init apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,263 @@
-import fs from 'fs';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
-console.log('hello', fs);
+const typeDefs = `
+  type User {
+    id: Int!
+    firstName: String!
+    lastName: String!
+    email: String!
+    username: String!
+    tasks: [Task]!
+  }
 
-console.log('something');
+  type Task {
+    id: Int!
+    title: String!
+    description: String
+    owners: [User]!
+    comments: [Comment]
+    createdBy: User!
+  }
+
+  type Comment {
+    id: Int!
+    content: String!
+    createdAt: String!
+    by: User!
+    task: Task!
+  }
+
+  type AuthPayload {
+    token: String!
+    user: User!
+  }
+
+  type Query {
+    users: [User]
+    tasks: [Task]
+    comments: [Comment]
+  }
+
+  type Mutation {
+    signup(input: SignupInput!): AuthPayload
+    login(email: String!, password: String!): AuthPayload
+    deleteUser(id: Int): [User]
+    addTask(task: TaskInput!): Task!
+    addComment(comment: CommentInput!): Comment!
+    deleteComment(id: Int!): Comment!
+  }
+
+  input SignupInput {
+    firstName: String!
+    lastName: String!
+    email: String!
+    username: String!
+    password: String!
+  }
+
+  input TaskInput {
+    title: String!
+    description: String
+  }
+
+  input CommentInput {
+    content: String!
+    task: Int!
+  }
+`;
+
+const resolvers = {
+  // Query: {
+  //     users: (parent, args, { prisma }) => {
+  //         return prisma.user.findMany({
+  //             include: { tasks: true },
+  //         });
+  //     },
+  //     tasks: (parent, args, { prisma }) => {
+  //         return prisma.task.findMany({
+  //             include: { owners: true },
+  //         });
+  //     },
+  // },
+  // Task: {
+  //     comments: (parent, args, { prisma }) => {
+  //         return prisma.task.findUnique({ where: { id: parent.id } }).comments();
+  //     },
+  //     owners: (parent, args, { prisma }) => {
+  //         return prisma.task.findUnique({ where: { id: parent.id } }).owners();
+  //     },
+  //     createdBy: (parent, args, { prisma }) => {
+  //         return prisma.task.findUnique({ where: { id: parent.id } }).createdBy();
+  //     },
+  // },
+  // Comment: {
+  //     task: (parent, args, { prisma }) => {
+  //         return prisma.comment.findUnique({ where: { id: parent.id } }).task();
+  //     },
+  //     by: (parent, args, { prisma }) => {
+  //         return prisma.comment.findUnique({ where: { id: parent.id } }).by();
+  //     },
+  // },
+  // Mutation: {
+  //     signup: async (parent, { input }, { prisma }) => {
+  //         const { password: plaintextPassword, ...userFields } = input;
+  //         const password = await hash(plaintextPassword, 10);
+  //         try {
+  //             const user = await prisma.user.create({
+  //                 data: {
+  //                     ...userFields,
+  //                     password,
+  //                 },
+  //             });
+  //
+  //             const token = sign({ userId: user.id }, process.env.APP_SECRET);
+  //
+  //             return {
+  //                 token,
+  //                 user,
+  //             };
+  //         } catch (error) {
+  //             throw new Error(error.message);
+  //         }
+  //     },
+  //     login: async (parent, { email, password }, { prisma }) => {
+  //         try {
+  //             const user = await prisma.user.findUnique({
+  //                 where: { email },
+  //             });
+  //
+  //             await checkUserLogin(user, password);
+  //             const token = sign({ userId: user.id }, process.env.APP_SECRET);
+  //
+  //             return {
+  //                 token,
+  //                 user,
+  //             };
+  //         } catch (error) {
+  //             throw new Error(error.message);
+  //         }
+  //     },
+  //     deleteUser: async (parent, { id }, { prisma }) => {
+  //         try {
+  //             return await prisma.user.delete({
+  //                 where: {
+  //                     id,
+  //                 },
+  //             });
+  //         } catch (error) {
+  //             throw new Error(error.message);
+  //         }
+  //     },
+  //     addTask: async (parent, { task }, { prisma, userId }) => {
+  //         try {
+  //             checkIfUserLoggedIn(userId);
+  //
+  //             return await prisma.task.create({
+  //                 data: {
+  //                     ...task,
+  //                     owners: {
+  //                         connect: [{ id: userId }],
+  //                     },
+  //                     createdBy: {
+  //                         connect: { id: userId },
+  //                     },
+  //                 },
+  //             });
+  //         } catch (error) {
+  //             throw new Error(error.message);
+  //         }
+  //     },
+  //     addComment: async (parent, { comment }, { prisma, userId }) => {
+  //         try {
+  //             checkIfUserLoggedIn(userId);
+  //
+  //             return await prisma.comment.create({
+  //                 data: {
+  //                     ...comment,
+  //                     by: {
+  //                         connect: { id: userId },
+  //                     },
+  //                     task: {
+  //                         connect: { id: comment.task },
+  //                     },
+  //                 },
+  //             });
+  //         } catch (error) {
+  //             throw new Error(error.message);
+  //         }
+  //     },
+  //     deleteComment: async (parent, { id }, { prisma, userId }) => {
+  //         try {
+  //             checkIfUserLoggedIn(userId);
+  //
+  //             return await prisma.comment.delete({
+  //                 where: {
+  //                     id,
+  //                 },
+  //             });
+  //         } catch (error) {
+  //             throw new Error(error.message);
+  //         }
+  //     },
+  // },
+  Query: {
+    users: () => [
+      {
+        id: 1,
+        firstName: 'Mahmoud',
+        lastName: 'Elawadi',
+        email: 'mahmoudwagdi86@gmail.com',
+        username: 'mwagdi',
+        tasks: [],
+      },
+    ],
+    tasks: () => [
+      {
+        id: 1,
+        title: 'Task',
+        description: 'Description',
+        owners: [
+          {
+            id: 1,
+            firstName: 'Mahmoud',
+            lastName: 'Elawadi',
+            email: 'mahmoudwagdi86@gmail.com',
+            username: 'mwagdi',
+            tasks: [],
+          },
+        ],
+        comments: [],
+        createdBy: {
+          id: 1,
+          firstName: 'Mahmoud',
+          lastName: 'Elawadi',
+          email: 'mahmoudwagdi86@gmail.com',
+          username: 'mwagdi',
+          tasks: [],
+        },
+      },
+    ],
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+// const { url } = await startStandaloneServer(server, {
+//   listen: { port: 4000 },
+// });
+
+(async () => {
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`🚀  Server ready at: ${url}`);
+})();
